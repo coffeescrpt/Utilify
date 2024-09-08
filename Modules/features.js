bac903c35641a97796a4e8571ccf6037dba47394
simon <143356794+unreallain@users.noreply.github.com>
2024-09-08 22:12:36 +0200

    Create features.js

---
+++ Modules/features.js
@@ -0,0 +1,27 @@
+// FEATURES:
+// - Allow Pastem
+// - Auto Block Users
+// - AutoFBadgeRedeem
+// - Better Titles
+// - Compact Menu
+// - Console Warning
+// - Creator Credits
+// - Display ST-notifications for incoming direct messages
+// - Edit Website Gradient
+// - Fast Friendslist
+// - Filter friendsbar to access DMs faster
+// - Find User Avatars (requested by idealism)
+// - Fixed profile background lightness
+// - Fix HTML syntax for ', >, "
+// - GetRidOfImageStrokes
+// - HeadersInDescription
+// - KoGaMaBuddy emojis
+// - Preview Marketplace Images
+// - PrivacyBlur V2
+// - RememberSafetyNotification
+// - RichText
+// - Steal Description
+// - User Backgrounds
+// - User Gradients
+// - View Feed (requested by Snowy)
+ 
